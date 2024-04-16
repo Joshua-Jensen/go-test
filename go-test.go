32c3202db65bdf5d3641a4a085414d961b262435
josh <joshjj3@gmail.com>
2024-04-16 16:55:57 -0600

    whats up

--- go-test.go
+++ go-test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gocolly/colly"
 )
@@ -9,7 +10,7 @@ import (
 // this is the typing for each scraped item to follow
 type item struct {
 	name        string
-	price       int
+	price       string
 	img         string
 	description string
 	size        string
@@ -20,13 +21,31 @@ func main() {
 		colly.Async(true),
 		colly.AllowedDomains("www.albertsons.com"),
 	)
-	var items []item
-	c.OnHTML("product-item-al", func(e *colly.HTMLElement) {
-		item := item{
-			price: e.ChildText(".product-price"),
-		}
-		items = append(items, item)
-	})
+	// var items []item
 
-	fmt.Println()
+	// c.OnHTML("product-item-al", func(e *colly.HTMLElement) {
+	// 	item := item{
+	// 		price: e.ChildText(".product-price"),
+	// 	}
+	// 	items = append(items, item)
+	// })
+
+
+
+c.OnRequest(func(r *colly.Request) {
+	fmt.Println("visiting",r.URL)
+})
+
+c.OnResponse(func(r *colly.Response) {
+	fmt.Println("response received", r.Request.URL )
+})
+
+c.OnError(func(r *colly.Response, err error) {
+	log.Println("whoops!",err)
+})
+
+c.Visit("https://www.albertsons.com/shop/search-results.html?q=egg")
+
+c.Wait()
+fmt.Println("scrapping done /n", document)
 }
